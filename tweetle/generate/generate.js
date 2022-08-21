ada3d8a67a53c0213a68b54917a62951e558be4f
Kevin Quigley <kquigley27@gmail.com>
2022-08-21 11:27:48 -0400

    add error handling to link generator

--- tweetle/generate/generate.js
+++ tweetle/generate/generate.js
@@ -13,11 +13,18 @@ function generateFunction(handle){
             console.log(responseText)
             resultsHTML = '<h5><img style="border-radius:50%;float:left" src="' + responseText.profilePicURL +  '"><span style="font-weight:bold;padding-left:8px;"><a style="color:black;" href="https://twitter.com/' + responseText.name +  '" target="_blank">' + responseText.userLabel + '</a></span><br><span style="font-size:75%;padding-left:8px;color:grey;"><a style="color:grey;" href="https://twitter.com/' + responseText.name +  '" target="_blank">@' + responseText.name + '</span></h5>';
             $('#searchResults').html(resultsHTML);
-            customLink = "https://tweetalyze.com/tweetle/?h=" + responseText.name;
-            username = responseText.name;
-            document.getElementById("play").style.display = "block";
-            document.getElementById("share").style.display = "block";
-            document.getElementById("copy").style.display = "block";
+            if(responseText.name != "User not found"){
+                customLink = "https://tweetalyze.com/tweetle/?h=" + responseText.name;
+                username = responseText.name;
+                document.getElementById("play").style.display = "block";
+                document.getElementById("share").style.display = "block";
+                document.getElementById("copy").style.display = "block";
+            }
+            else{
+                document.getElementById("play").style.display = "none";
+                document.getElementById("share").style.display = "none";
+                document.getElementById("copy").style.display = "none";
+            }
         },
         error:function(error){
             console.log("Error");
